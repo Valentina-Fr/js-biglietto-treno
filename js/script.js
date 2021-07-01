07428246817350871313efd0ed6eb3be9f9db038
Valentina-Fr <v_frasca@virgilio.it>
2021-07-01 15:57:46 +0200

    Add var total price

--- js/script.js
+++ js/script.js
@@ -23,3 +23,21 @@ console.log('sconto 20%: ', discount20);
 console.log('sconto 40%: ', discount40);
 console.log('elemento html: ', result);
 console.log(message);
+
+var firstTot
+
+if (age < 18) {
+    console.log(firstTot = price - discount20)
+} else if (age > 65) {
+    console.log(firstTot = price - discount40)
+} else {
+    console.log(firstTot = price)
+}
+
+//Abbreviare a due decimali
+var abbrTot = firstTot.toFixed(2);
+console.log('totale a due decimali: ', abbrTot);
+
+//Convertire in stringa
+var finalTot = abbrTot.toString();
+console.log(typeof finalTot);
